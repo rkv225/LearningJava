23e756e3e03009bb412c89456bf4fedefbda1789
rishabhvarshney <rishabh.varshney@us.nationalgrid.com>
2025-02-13 08:00:29 +0530

    Contructors

--- src/main/java/AccessModifiers/Main.java
+++ src/main/java/AccessModifiers/Main.java
@@ -4,20 +4,6 @@ package AccessModifiers;
 // which means that all members are visible within the same package,
 // but aren’t accessible from other packages
 
-public class SuperPublic {
-    static void defaultMethod() { // if we make this as public, it will be accessible from anywhere
-        System.out.println("SuperPublic.defaultMethod");
-    }
-
-    private static void privateMethod() {
-        System.out.println("SuperPublic.privateMethod");
-    }
-
-    private void anotherPrivateMethod() {
-        privateMethod(); // available in the same class only.
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         SuperPublic.defaultMethod(); // Available in the same package
