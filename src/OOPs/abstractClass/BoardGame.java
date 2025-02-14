d1c8b34a1e7a00bef7afcc34ced3e9a9d1e30cff
rishabhvarshney <rishabh.varshney@us.nationalgrid.com>
2025-02-14 13:56:07 +0530

    abstract class

---
+++ src/OOPs/abstractClass/BoardGame.java
@@ -0,0 +1,5 @@
+package OOPs.abstractClass;
+
+public abstract class BoardGame {
+    public abstract void play();
+}
---
+++ src/OOPs/abstractClass/Checkers.java
@@ -0,0 +1,8 @@
+package OOPs.abstractClass;
+
+public class Checkers extends BoardGame{
+    @Override
+    public void play() {
+        System.out.println("Checkers");
+    }
+}
