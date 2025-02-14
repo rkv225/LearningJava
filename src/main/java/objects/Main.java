0c4b5075879dfac9f3c6f588eccc0b154a25f9a1
rishabhvarshney <rishabh.varshney@us.nationalgrid.com>
2025-02-14 13:31:38 +0530

    objects

---
+++ src/main/java/objects/Main.java
@@ -0,0 +1,19 @@
+package objects;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int id; // declaration of variable
+        id = 1; // initialization of variable
+
+        User u; // currently having null value
+        u = new User(); // creating object of type User via calling constructor
+
+        /*
+        Order of initialization:
+        static variables and static initializers in order
+        instance variables and instance initializers in order
+        constructors
+         */
+    }
+}
